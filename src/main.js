70622eb46f588c9ab5938db1b181590778aaf313
Jean Jia <jean84646@gmail.com>
2018-08-10 14:55:46 -0700

    Add search by medical issue and display doctor's name, address, phone, website, and wether accepting new patients.

--- src/main.js
+++ src/main.js
@@ -4,17 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { doctorLookup } from './doctorLookup.js';
 
 $(document).ready(function() {
-  $('#issue-form').submit(function(event) {
+  $('#search-form').submit(function(event) {
     event.preventDefault();
+    let name = $('#inputName').val();
     let issue = $('#inputIssue').val();
     let newLookup = new doctorLookup();
-    let promise = newLookup.search(issue);
+    let promise = newLookup.searchByIssue(issue);
 
     promise.then(function(response) {
       let body = JSON.parse(response);
       console.log(body);
-      // $('#showListOfDocByIssue').append("<li>" +  + "</li>");
-      // $('#showListOfDocByName').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+      body.data.forEach(function(item) {
+        let name = item.profile.first_name + " " + item.profile.last_name + ", " + item.profile.title;
+        let address = item.practices[0].visit_address.street + " " + item.practices[0].visit_address.street2 + ", "
+        + item.practices[0].visit_address.city + ", " + item.practices[0].visit_address.state + ", " + item.practices[0].visit_address.zip;
+        let phone = item.practices[0].phones[0].number;
+        let website = item.practices[0].website;
+        let acceptPt = item.practices[0].accepts_new_patients;
+        $('#showListOfDoctors').append("<li>" + name + "<br>" + address + "<br>" + phone + "<br>" + "This doctor is accepting new patient: " + acceptPt + "<br>" + "Website: " + website + "</li>");
+      })
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });

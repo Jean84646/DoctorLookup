3610ec821cb642a129e3ba7ec277278198a90ebf
Jean Jia <jean84646@gmail.com>
2018-08-10 16:07:37 -0700

    Add css styles.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css'
 import { doctorLookup } from './doctorLookup.js';
 
 $(document).ready(function() {

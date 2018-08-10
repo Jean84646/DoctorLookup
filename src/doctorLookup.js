0af4a1e91acaddcd78ae3ee6e945f85c30e96794
Jean Jia <jean84646@gmail.com>
2018-08-10 13:15:34 -0700

    Add index, business, and user interface files. apiKey working.

---
+++ src/doctorLookup.js
@@ -0,0 +1,17 @@
+export class doctorLookup {
+  search(issue) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { doctorLookup } from './doctorLookup.js';
+
+$(document).ready(function() {
+  $('#issue-form').submit(function(event) {
+    event.preventDefault();
+    let issue = $('#inputIssue').val();
+    let newLookup = new doctorLookup();
+    let promise = newLookup.search(issue);
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      console.log(body);
+      // $('#showListOfDocByIssue').append("<li>" +  + "</li>");
+      // $('#showListOfDocByName').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+});
